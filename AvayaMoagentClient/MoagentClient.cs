8327e23b3510a02a55d134381aec449327c7a73f
Matt Little <matt@matthewjlittle.com>
2010-10-04 12:19:11 -0400

    Rework MoagentClient to use OpenSSL
    --HG--
    branch : openssl

--- AvayaMoagentClient/MoagentClient.cs
+++ AvayaMoagentClient/MoagentClient.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using OpenSSL;
+using OpenSSL.Core;
+using OpenSSL.X509;
 
 namespace AvayaMoagentClient
 {
@@ -13,6 +17,7 @@ namespace AvayaMoagentClient
   public class StateObject
   {
     public Socket WorkSocket { get; set; }
+    public SslStream SecureStream { get; set; }
     public const int BufferSize = 256;
     public byte[] buffer = new byte[BufferSize];
     public StringBuilder sb = new StringBuilder();
@@ -20,12 +25,14 @@ namespace AvayaMoagentClient
 
   public class MoagentClient
   {
-
     private int _invokeIdSequence = 1;
     private static Message lastMsg;
     private Socket _client;
+    private SslStream _sslWrapper;
     private string _server;
     private int _port;
+    private X509List _xList;
+    private X509Chain _xChain;
 
     public delegate void MessageReceivedHandler(object sender, MessageReceivedEventArgs e);
 
@@ -36,8 +43,19 @@ namespace AvayaMoagentClient
     {
       _server = host;
       _port = port;
-
+      
       _client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+      var certBio = BIO.File(@"C:\windows\MoagentCertificates\agentClientCert.p12", "r");
+      X509Certificate clientCert = X509Certificate.FromPKCS12(certBio, string.Empty);
+      var serverBio = BIO.File(@"C:\windows\MoagentCertificates\ProactiveContactCA.cer", "r");
+      X509Certificate serverCert = X509Certificate.FromDER(serverBio);
+
+      _xList = new X509List();
+      _xList.Add(clientCert);
+      _xChain = new X509Chain();
+      _xChain.Add(clientCert);
+      _xChain.Add(serverCert);
     }
 
     public void StartClient(string host, string port)
@@ -103,6 +121,7 @@ namespace AvayaMoagentClient
       var ip = IPAddress.Parse(_server);
       var remoteEp = new IPEndPoint(ip, _port);
       _client.BeginConnect(remoteEp, ConnectCallback, _client);
+
       //_client.BeginConnect("mlittle.acttoday.com", 22700, ConnectCallback, _client);
     }
 
@@ -112,7 +131,25 @@ namespace AvayaMoagentClient
 
       client.EndConnect(ar);
 
-      Receive(client);
+      var stream = new NetworkStream(_client, FileAccess.ReadWrite, true);
+      _sslWrapper = new SslStream(stream, false, ValidateRemoteCert, clientCertificateSelectionCallback);
+
+      //var certBio = BIO.File(@"C:\windows\MoagentCertificates\agentClientCert.p12", "r");
+      //X509Certificate clientCert = X509Certificate.FromPKCS12(certBio, string.Empty);
+      //var serverBio = BIO.File(@"C:\windows\MoagentCertificates\ProactiveContactCA.cer", "r");
+      //X509Certificate serverCert = X509Certificate.FromDER(serverBio);
+
+      //var xList = new X509List();
+      //xList.Add(clientCert);
+      ////xList.Add(serverCert);
+      //var xChain = new X509Chain();
+      //xChain.Add(clientCert);
+      //xChain.Add(serverCert);
+
+      _sslWrapper.AuthenticateAsClient("192.168.80.79", _xList, _xChain, SslProtocols.Default, SslStrength.All,
+                                       false);
+
+      Receive(_sslWrapper);
 
       if (ConnectComplete != null)
         ConnectComplete(this, EventArgs.Empty);
@@ -120,8 +157,13 @@ namespace AvayaMoagentClient
 
     public void Disconnect()
     {
-      _client.Shutdown(SocketShutdown.Both);
-      _client.Close();
+      _sslWrapper.Close();
+      //_client.Shutdown(SocketShutdown.Both);
+      if (_client.Connected)
+        _client.Close();
+      
+      _sslWrapper.Dispose();
+      _sslWrapper = null;
     }
 
     //private void GetResponse(Socket client)
@@ -147,14 +189,14 @@ namespace AvayaMoagentClient
     //  }
     //}
 
-    private void Receive(Socket client)
+    private void Receive(SslStream client)
     {
       try
       {
         var state = new StateObject();
-        state.WorkSocket = client;
-
-        client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+        state.SecureStream = client;
+        
+        client.BeginRead(state.buffer, 0, StateObject.BufferSize, new AsyncCallback(ReceiveCallback), state);
       }
       catch (Exception e)
       {
@@ -171,12 +213,13 @@ namespace AvayaMoagentClient
         // Retrieve the state object and the client socket 
         // from the asynchronous state object.
         var state = (StateObject)ar.AsyncState;
-        var handler = state.WorkSocket;
+        var handler = state.SecureStream;
 
-        if (handler.Connected)
+        if (handler.CanRead)
         {
           // Read data from the remote device.
-          int bytesRead = handler.EndReceive(ar);
+          int bytesRead = handler.EndRead(ar);
+          //int bytesRead = handler.EndReceive(ar);
 
           if (bytesRead > 0)
           {
@@ -210,7 +253,7 @@ namespace AvayaMoagentClient
             }
           }
 
-          handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+          handler.BeginRead(state.buffer, 0, StateObject.BufferSize, new AsyncCallback(ReceiveCallback), state);
         }
       }
       catch (Exception e)
@@ -246,7 +289,8 @@ namespace AvayaMoagentClient
       byte[] byteData = Encoding.ASCII.GetBytes(data);
 
       // Begin sending the data to the remote device.
-      _client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), _client);
+      _sslWrapper.BeginWrite(byteData, 0, byteData.Length, new AsyncCallback(SendCallback), _sslWrapper);
+      //_client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), _client);
     }
 
     private static void SendCallback(IAsyncResult ar)
@@ -254,14 +298,115 @@ namespace AvayaMoagentClient
       try
       {
         // Retrieve the socket from the state object.
-        var client = (Socket)ar.AsyncState;
-
+        var client = (SslStream)ar.AsyncState;
+        client.EndWrite(ar);
         //TODO: Tell somebody?
       }
       catch (Exception e)
       {
         Console.WriteLine(e.ToString());
       }
+    }
+
+    bool ValidateRemoteCert(object obj, X509Certificate cert, X509Chain chain, int depth, VerifyResult result)
+    {
+      bool ret = false;
+
+      switch (result)
+      {
+        case VerifyResult.X509_V_ERR_CERT_UNTRUSTED:
+        case VerifyResult.X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT:
+        case VerifyResult.X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY:
+        case VerifyResult.X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE:
+          {
+            // Check the chain to see if there is a match for the cert
+            ret = CheckCert(cert, chain);
+            if (!ret && depth != 0)
+            {
+              ret = true;  // We want to keep checking until we get to depth 0
+            }
+          }
+          break;
+        case VerifyResult.X509_V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD:
+        case VerifyResult.X509_V_ERR_CERT_NOT_YET_VALID:
+          {
+            Console.WriteLine("Certificate is not valid yet");
+            ret = false;
+          }
+          break;
+        case VerifyResult.X509_V_ERR_CERT_HAS_EXPIRED:
+        case VerifyResult.X509_V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD:
+          {
+            Console.WriteLine("Certificate is expired");
+            ret = false;
+          }
+          break;
+        case VerifyResult.X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT:
+          {
+            // we received a self signed cert - check to see if it's in our store
+            ret = CheckCert(cert, chain);
+          }
+          break;
+        case VerifyResult.X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN:
+          {
+            // A self signed certificate was encountered in the chain
+            // Check the chain to see if there is a match for the cert
+            ret = CheckCert(cert, chain);
+            if (!ret && depth != 0)
+            {
+              ret = true;  // We want to keep checking until we get to depth 0
+            }
+          }
+          break;
+        case VerifyResult.X509_V_OK:
+          {
+            ret = true;
+          }
+          break;
+      }
+      return ret;
+    }
+
+    protected X509Certificate clientCertificateSelectionCallback(object sender, string targetHost, X509List localCerts, X509Certificate remoteCert, string[] acceptableIssuers)
+    {
+      X509Certificate retCert = null;
+
+      // check target host?
+
+      for (int i = 0; i < acceptableIssuers.GetLength(0); i++)
+      {
+        X509Name name = new X509Name(acceptableIssuers[i]);
+
+        foreach (X509Certificate cert in localCerts)
+        {
+          if (cert.Issuer.CompareTo(name) == 0)
+          {
+            retCert = cert;
+            break;
+          }
+          cert.Dispose();
+        }
+        name.Dispose();
+      }
+      return retCert;
+    }
+
+    bool CheckCert(X509Certificate cert, X509Chain chain)
+    {
+      if (cert == null || chain == null)
+      {
+        return false;
+      }
+
+      foreach (X509Certificate certificate in chain)
+      {
+        if (cert == certificate)
+        {
+          return true;
+        }
+      }
+
+      return false;
     }
   }
 }

f2189f303b5681b308c839cd2e1f9af280ae7b71
Matt Little <matt@matthewjlittle.com>
2010-10-08 17:45:32 -0400

    new command
    --HG--
    branch : openssl

---
+++ AvayaMoagentClient/Commands/FreeHeadset.cs
@@ -0,0 +1,15 @@
+﻿namespace AvayaMoagentClient.Commands
+{
+  public class FreeHeadset : Message
+  {
+    private const string COMMAND = "AGTFreeHeadset";
+
+    public FreeHeadset()
+      : this(false)
+    { }
+
+    public FreeHeadset(bool cacheRawMessage)
+      : base(COMMAND, MessageType.Command, cacheRawMessage)
+    { }
+  }
+}

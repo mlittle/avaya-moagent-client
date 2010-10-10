22612e2e9d5aff698d16f95e168232cc2e919fe8
Matt Little <matt@matthewjlittle.com>
2010-10-09 20:28:42 -0400

    oops need the file too
    --HG--
    branch : openssl

---
+++ AvayaMoagentClient/Commands/ManagedCall.cs
@@ -0,0 +1,15 @@
+﻿namespace AvayaMoagentClient.Commands
+{
+  public class ManagedCall : Message
+  {
+    private const string COMMAND = "AGTManagedCall";
+
+    public ManagedCall()
+      : this(false)
+    { }
+
+    public ManagedCall(bool cacheRawMessage)
+      : base(COMMAND, MessageType.Command, cacheRawMessage)
+    { }
+  }
+}

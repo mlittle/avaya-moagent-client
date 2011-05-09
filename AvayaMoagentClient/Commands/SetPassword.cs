d2e55aa438e2a6540aa714393547f0bf01d64b72
Matt Little <matt@matthewjlittle.com>
2011-05-09 18:40:18 -0400

    Add Set Password Message; Should allow us to update the passwords for agents when they expire by changing it to a temp password and back to the original
    --HG--
    branch : openssl

---
+++ AvayaMoagentClient/Commands/SetPassword.cs
@@ -0,0 +1,15 @@
+﻿namespace AvayaMoagentClient.Commands
+{
+  public class SetPassword : Message
+  {
+    private const string COMMAND = "AGTSetPassword";
+
+    public SetPassword(string userId, string presentPassword, string newPassword)
+      : this(userId, presentPassword, newPassword, false)
+    { }
+
+    public SetPassword(string userId, string presentPassword, string newPassword, bool cacheRawMessage)
+      : base(COMMAND, MessageType.Command, cacheRawMessage, userId, presentPassword, newPassword)
+    { }
+  }
+}
